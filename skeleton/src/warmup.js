a8768b422fd8937810b3641dfba6792e20b55054
mfong415 <85459923+mfong415@users.noreply.github.com>
2022-11-30 14:43:58 -0800

    First

---
+++ skeleton/src/warmup.js
@@ -0,0 +1,11 @@
+
+const partyHeader = document.getElementById('party');
+let newP;
+
+//export const htmlGenerator = (string, htmlElement) => {
+//    newP = document.createElement(htmlElement);
+//    newP.innerHTML = string;
+//    partyHeader.append(newP);
+//};
+//
+//htmlGenerator('Party Time.', partyHeader);
